1283d71e49754dd4e7d0553f39ab2e13cc985f5f
DaNub <75505891+DaNubCoding@users.noreply.github.com>
2023-12-13 10:05:13 -0500

    proper player test image

--- Player.java
+++ Player.java
@@ -2,7 +2,7 @@ import greenfoot.*;
 
 public class Player extends Sprack {
     public Player(MainGame scene) {
-        super(scene, Greenfoot.getRandomNumber(scene.w), Greenfoot.getRandomNumber(scene.h), "player_test.png", 16);
+        super(scene, Greenfoot.getRandomNumber(scene.w), Greenfoot.getRandomNumber(scene.h), "player.png", 32);
     }
     
     public void tick() {
