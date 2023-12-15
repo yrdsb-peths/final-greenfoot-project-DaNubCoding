c4b18b8ef5111a19b97e7b41c96aea9f79dde66f
DaNub <75505891+DaNubCoding@users.noreply.github.com>
2023-12-14 22:25:39 -0500

    more consistent use of this in SprackGroup

--- SprackGroup.java
+++ SprackGroup.java
@@ -1,6 +1,8 @@
 import greenfoot.*;
 
 public class SprackGroup {
+    private String path;
+    private int numOfLayers;
     private GreenfootImage[] rotationImages = new GreenfootImage[360];
     private int fullWidth;
     private int fullHeight;
@@ -11,7 +13,9 @@ public class SprackGroup {
     private int diagonal;
     
     public SprackGroup(String path, int numOfLayers) {
-        this.layerImages = this.parseSpritesheet(path, numOfLayers);
+        this.path = path;
+        this.numOfLayers = numOfLayers;
+        this.parseSpritesheet();
         
         this.w = this.layerImages[0].getWidth();
         this.h = this.layerImages[0].getHeight();
@@ -23,40 +27,40 @@ public class SprackGroup {
         this.createLayers();
     }
     
-    private static GreenfootImage[] parseSpritesheet(String path, int numOfLayers) {
-        GreenfootImage[] layerImages = new GreenfootImage[numOfLayers];
-        GreenfootImage spritesheet = new GreenfootImage(path);
+    private void parseSpritesheet() {
+        this.layerImages = new GreenfootImage[this.numOfLayers];
+        GreenfootImage spritesheet = new GreenfootImage(this.path);
         spritesheet.mirrorVertically();
         int sheetWidth = spritesheet.getWidth(), sheetHeight = spritesheet.getHeight();
-        int layerWidth = sheetWidth, layerHeight = sheetHeight / numOfLayers;
+        int layerWidth = sheetWidth, layerHeight = sheetHeight / this.numOfLayers;
         
-        for (int i = 0; i < numOfLayers; i++) {
+        for (int i = 0; i < this.numOfLayers; i++) {
             GreenfootImage layerImage = new GreenfootImage(layerWidth, layerHeight);
             layerImage.drawImage(spritesheet, 0, -i * layerHeight);
-            layerImages[i] = layerImage;
+            this.layerImages[i] = layerImage;
             layerImage.scale(layerImage.getWidth() * Scene.PX, layerImage.getHeight() * Scene.PX);
         }
-        
-        return layerImages;
     }
     
     private void createLayers() {
-        for (int i = 0; i < layers.length; i++) {
-            GreenfootImage image = getLayerImage(i);
-            layers[i] = new SprackLayer(i * Scene.PX, fullHeight, diagonal, image);
+        for (int i = 0; i < this.layers.length; i++) {
+            GreenfootImage image = this.getLayerImage(i);
+            this.layers[i] = new SprackLayer(i * Scene.PX, this.fullHeight, this.diagonal, image);
         }
     }
     
     public GreenfootImage getLayerImage(int layer) {
-        return layerImages[layer];
+        return this.layerImages[layer];
     }
     
     private GreenfootImage generateRotationImage(int angle) {
-        GreenfootImage image = rotationImages[angle] = new GreenfootImage(fullWidth, fullHeight);
-        for (int i = 0; i < layers.length; i++) {
-            layers[i].rotate(angle);
+        this.rotationImages[angle] = new GreenfootImage(fullWidth, fullHeight);
+        GreenfootImage image = this.rotationImages[angle];
+        for (int i = 0; i < this.layers.length; i++) {
+            SprackLayer layer = this.layers[i];
+            layer.rotate(angle);
             for (int j = 0; j < Scene.PX; j++) {
-                image.drawImage(layers[i].getImage(), layers[i].getX(), layers[i].getY() - j);
+                image.drawImage(layer.getImage(), layer.getX(), layer.getY() - j);
             }
         }
         return image;
