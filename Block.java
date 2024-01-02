22973dec94bcbde4dd1f3b29a7d3c05ead037301
DaNubCoding <danubcoding@gmail.com>
2024-01-02 16:39:29 -0500

    changes to sorting and the re-addition of the player

--- Block.java
+++ Block.java
@@ -1,8 +1,8 @@
 import greenfoot.*;
 
 public class Block extends Sprack {
-    public Block(MainGame scene, SprackGroup group, int x, double y, int z) {
-        super(scene, group, x * 16 * scene.PX, (int) (y * 16 * scene.PX), z * 16 * scene.PX);
+    public Block(MainGame scene, int x, int y, int z) {
+        super(scene, scene.blockSprackGroup, x * 16 * scene.PX, y * 16 * scene.PX, z * 16 * scene.PX);
     }
     
     public void tick() {
--- Camera.java
+++ Camera.java
@@ -1,84 +1,15 @@
 import greenfoot.*;
 
 public class Camera extends Sprite<MainGame> {
-    private DummyPlayer player;
+    private Player player;
     private Vector3 pos;
     private double horAngle;
     private double verAngle;
     
-    private class DummyPlayer extends Sprite<MainGame> {
-        private Vector3 pos;
-        protected int horAngle;
-        protected int verAngle;
-        
-        public DummyPlayer(MainGame scene, int x, int y, int z) {
-            super(scene, x, z);
-            this.pos = new Vector3(x, y, z);
-            this.verAngle = 45;
-            this.setImage((GreenfootImage) null);
-        }
-        
-        public Vector3 getPos() {
-            return this.pos;
-        }
-        
-        public int getHorAngle() {
-            return this.horAngle;
-        }
-        
-        public int getVerAngle() {
-            return this.verAngle;
-        }
-        
-        public void tick() {
-            if (Greenfoot.isKeyDown("w")) { // up
-                this.pos.xz.plus$(new Vector2(0, -5).rotate(this.horAngle));
-            }
-            if (Greenfoot.isKeyDown("s")) { // down
-                this.pos.xz.plus$(new Vector2(0, 5).rotate(this.horAngle));
-            }
-            if (Greenfoot.isKeyDown("a")) { // left
-                this.pos.xz.plus$(new Vector2(-5, 0).rotate(this.horAngle));
-            }
-            if (Greenfoot.isKeyDown("d")) { // right
-                this.pos.xz.plus$(new Vector2(5, 0).rotate(this.horAngle));
-            }
-            if (Greenfoot.isKeyDown("space")) { // up
-                this.pos.y.plus$(5);
-            }
-            if (Greenfoot.isKeyDown("shift")) { // down
-                this.pos.y.minus$(5);
-            }
-            
-            if (this.pos.z.get() > 9 * 64 - 32 - 19.2) {
-                this.pos.z.set(9 * 64 - 32 - 19.2);
-            }
-            
-            if (Greenfoot.isKeyDown("left")) { // CCW
-                this.horAngle -= 2;
-            }
-            if (Greenfoot.isKeyDown("right")) { // CW
-                this.horAngle += 2;
-            }
-            if (Greenfoot.isKeyDown("up")) {
-                this.verAngle += 1;
-                if (this.verAngle > 45) {
-                    this.verAngle = 45;
-                }
-            }
-            if (Greenfoot.isKeyDown("down")) {
-                this.verAngle -= 1;
-                if (this.verAngle < 13) {
-                    this.verAngle = 13;
-                }
-            }
-        }
-    }
-    
     public Camera(MainGame scene) {
         super(scene, 0, 0);
         this.setImage((GreenfootImage) null);
-        this.player = new DummyPlayer(scene, 0, 0, 0);
+        this.player = scene.player;
         this.pos = Vector3.ZERO;
         this.horAngle = 0;
         this.verAngle = 45;
