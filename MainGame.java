33264730c9ee4acc67d61e8d0c8bd09b13245465
DaNubCoding <danubcoding@gmail.com>
2024-01-10 20:58:52 -0500

    add dirt blocks

--- MainGame.java
+++ MainGame.java
@@ -2,14 +2,17 @@ import greenfoot.*;
 import java.util.*;
 
 public class MainGame extends Scene {
-    public static SprackGroup blockSprackGroup;
+    public static SprackGroup grassSprackGroup;
+    public static SprackGroup dirtSprackGroup;
     public static SprackGroup playerSprackGroup;
     
     static {
         Log.start();
         Log.debug("Loading static sprack groups:");
-        Log.debug("Creating sprack group from \"block.png\"...");
-        blockSprackGroup = new SprackGroup("block.png", 16);
+        Log.debug("Creating sprack group from \"grass.png\"...");
+        grassSprackGroup = new SprackGroup("grass.png", 16);
+        Log.debug("Creating sprack group from \"dirt.png\"...");
+        dirtSprackGroup = new SprackGroup("dirt.png", 16);
         Log.debug("Creating sprack group from \"player.png\"...");
         playerSprackGroup = new SprackGroup("player.png", 16);
     }
@@ -25,25 +28,25 @@ public class MainGame extends Scene {
         Log.debug("Spawning spracks...");
         for (int x = 0; x < 12; x++) {
             for (int z = 0; z < 12; z++) {
-                new Block(this, x, 0, z);
+                new Block(this, grassSprackGroup, x, 0, z);
             }
         }
-        new Block(this, 9, 1, 3);
-        new Block(this, 3, 1, 4);
-        new Block(this, 4, 1, 4);
-        new Block(this, 5, 1, 4);
-        new Block(this, 4, 1, 3);
-        new Block(this, 4, 1, 5);
-        new Block(this, 4, 2, 4);
+        new Block(this, grassSprackGroup, 9, 1, 3);
+        new Block(this, grassSprackGroup, 3, 1, 4);
+        new Block(this, grassSprackGroup, 4, 1, 4);
+        new Block(this, grassSprackGroup, 5, 1, 4);
+        new Block(this, grassSprackGroup, 4, 1, 3);
+        new Block(this, grassSprackGroup, 4, 1, 5);
+        new Block(this, grassSprackGroup, 4, 2, 4);
         for (int x = 7; x < 10; x++) {
             for (int y = 1; y < 4; y++) {
-                new Block(this, x, y, 9);
+                new Block(this, dirtSprackGroup, x, y, 9);
             }
         }
-        new Block(this, 7, 3, 8);
-        new Block(this, 8, 3, 8);
-        new Block(this, 9, 3, 8);
-        new Block(this, 8, 2, 6);
+        new Block(this, grassSprackGroup, 7, 3, 8);
+        new Block(this, grassSprackGroup, 8, 3, 8);
+        new Block(this, grassSprackGroup, 9, 3, 8);
+        new Block(this, grassSprackGroup, 8, 2, 6);
         
         this.player = new Player(this);
         this.camera = new Camera(this);
