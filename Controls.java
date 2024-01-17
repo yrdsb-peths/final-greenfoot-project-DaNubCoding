8e474f31d6029e58ba1c31ab4f8d1e16efa576ff
DaNubCoding <danubcoding@gmail.com>
2024-01-17 16:49:46 -0500

    tweak the look of controls menu

--- Controls.java
+++ Controls.java
@@ -10,7 +10,7 @@ public class Controls extends Sprite<MainGame>
     public Controls(MainGame scene) {
         super(scene, scene.w / 2, scene.h / 2);
         this.setImage(new GreenfootImage("controls.png"));
-        this.image.setTransparency(150);
+        this.image.setTransparency(200);
     }
     
     public void toggle() {
